3c32621f6cd71a1f1eb04a485fadf17b55a501d0
Matt Volonnino <mvolonnino12@gmail.com>
2020-08-20 23:27:54 -0400

    delete by id- got post and get methods working and delete all and delete by the id using postman

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -12,10 +12,35 @@ module.exports = function (app) {
 
   app.post("/api/notes", function (req, res) {
     console.log("req.body: ", req.body);
-    dbJSON.push(req.body);
-    res.json(true);
+    // creating new const to add 1 to the last id in the dbJSON
+    const newID = dbJSON[dbJSON.length - 1].id + 1;
+    console.log("newID: ", newID);
+    // creating a new obj with the newid, ... takes the old obj and allows us to add an id
+    const newJSON = {
+      ...req.body,
+      id: newID,
+    };
+    console.log("newJSON: ", newJSON);
+    dbJSON.push(newJSON);
+    res.json({ dbJSON });
   });
 
+  // delete by ID
+  app.post("/api/notes/:id", function (req, res) {
+    const { id } = req.params;
+    // create a new array by filtering through the old array, if the delete id we click doesnt equal the id found in dbjson, filter it out of the old array and create the new filteredID arr
+    const filteredId = dbJSON.filter(function (jsonObj) {
+      console.log("jsonObj.id: ", jsonObj.id);
+      console.log("id: ", parseInt(id));
+      return jsonObj.id !== parseInt(id);
+    });
+
+    console.log("filteredId: ", filteredId);
+    dbJSON = filteredId;
+    res.json(filteredId);
+  });
+
+  // Delete all btn
   app.post("/api/clear", function (req, res) {
     dbJSON = [];
 
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,18 +1,16 @@
 // DEPENDENCIES
 const path = require("path");
 
-function htmlRoutes(app){
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page
 
-  app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
-  })
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-  })
-}
-
-module.exports = htmlRoutes;
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
